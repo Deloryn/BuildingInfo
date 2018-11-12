3f358b564a6e4e2fd8e53ab8a96cf33804ecafe1
Krzysztof Michalak <krzysztof.michalak96@gmail.com>
2018-11-12 21:58:55 +0100

    Repair the previous change

--- src/main/java/pl/put/poznan/buildingInfo/app/BuildingInfoApplication.java
+++ src/main/java/pl/put/poznan/buildingInfo/app/BuildingInfoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication(scanBasePackages = {"pl.put.poznan.buildingInfo"})
 public class BuildingInfoApplication {
 
-    public sta\tic void main(String[] args) {
+    public static void main(String[] args) {
         SpringApplication.run(BuildingInfoApplication.class, args);
     }
 }
